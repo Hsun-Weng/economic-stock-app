4f3f855d1b9f78952713e06945c5d5ba3a9f2e55
Hsun.Weng <>
2020-05-19 23:40:45 +0800

    Return 401 when login failed &

--- economic-app/src/main/java/com/hsun/economic/handler/JWTAuthenticationFailureHandler.java
+++ economic-app/src/main/java/com/hsun/economic/handler/JWTAuthenticationFailureHandler.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -27,7 +28,8 @@ public class JWTAuthenticationFailureHandler implements AuthenticationFailureHan
         
         responseBean.setStatus(0);
         responseBean.setMessage("Please confirm User Name or Password");
-        
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.getWriter().write(gson.toJson(responseBean));
         response.getWriter().flush();;
     }
