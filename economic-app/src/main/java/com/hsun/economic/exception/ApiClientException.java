aef43456e81bd7eb7f9eb54f887b79f57ec3adca
Hsun.Weng <>
2020-06-14 22:22:54 +0800

    Add Exception handler

--- economic-app/src/main/java/com/hsun/economic/exception/ApiClientException.java
+++ economic-app/src/main/java/com/hsun/economic/exception/ApiClientException.java
@@ -4,7 +4,6 @@ import com.hsun.economic.bean.ResponseBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ApiClientException extends RuntimeException {
 
     public ApiClientException(String message) {
--- economic-app/src/main/java/com/hsun/economic/exception/ApiServerException.java
+++ economic-app/src/main/java/com/hsun/economic/exception/ApiServerException.java
@@ -3,7 +3,6 @@ package com.hsun.economic.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class ApiServerException extends RuntimeException {
 
     public ApiServerException() {
