3850fd84c2257af64a0c5c21477f4f128fae1170
weihsunweng <weihsun.weng@pmi-ai.com>
2020-09-22 17:56:14 +0800

    Stock collection add parsing 'change' column and add paging stock sort  API.

--- data-app/src/main/java/com/hsun/data/repository/StockRepository.java
+++ data-app/src/main/java/com/hsun/data/repository/StockRepository.java
@@ -3,14 +3,18 @@ package com.hsun.data.repository;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hsun.data.entity.Stock;
 
 @Repository
-public interface StockRepository extends MongoRepository<Stock, String>{
+public interface StockRepository extends PagingAndSortingRepository<Stock, String> {
     @Query("{ 'stockCode': ?0, 'date': { $gte: ?1, $lte: ?2} }")
     List<Stock> findByStockCodeAndDateBetween(String stockCode, Date startDate, Date endDate);
+    Page<Stock> findByDateBetween(Date startDate, Date endDate, Pageable pageable);
 }
--- economic-python/get_stock_daily.py
+++ economic-python/get_stock_daily.py
@@ -30,6 +30,14 @@ def convert_price_text(price_text):
         pass
     return price
 
+def convert_change_text(change_text):
+    change = None
+    try:
+        change = float(change_text.replace('+', '').replace('X', '').replace(',', ''))
+    except ValueError:
+        pass
+    return change
+
 # 轉換成交量資料型態至float
 def convert_volume_value(volume_text):
     volume = None
@@ -46,17 +54,24 @@ def format_stock_data(date, stock_raw_data):
     format_stock_data['date'] = datetime.strptime(datetime_str, '%Y/%m/%d %H:%M:%S')
     format_stock_data['stock_code'] = stock_raw_data[0]
     format_stock_data['volume'] = convert_volume_value(stock_raw_data[2])
+    format_stock_data['close'] = convert_price_text(stock_raw_data[7])
     
-    if format_stock_data['volume'] > 0: # 成交量為0價格全填null(公開資料是為0)
+    if format_stock_data['volume'] > 0 and format_stock_data['close'] > 0: # 成交量為0或收盤價為0價格全填null(公開資料是為0)
         format_stock_data['open'] = convert_price_text(stock_raw_data[4])
         format_stock_data['high'] = convert_price_text(stock_raw_data[5])
         format_stock_data['low'] = convert_price_text(stock_raw_data[6])
-        format_stock_data['close'] = convert_price_text(stock_raw_data[7])
+
+        format_stock_data['change'] = convert_change_text(stock_raw_data[8])
+        ## 前一天收盤價
+        previous_close = format_stock_data['close'] + format_stock_data['change']
+        format_stock_data['change_percent'] = round(format_stock_data['change'] / previous_close, 4)
     else: 
         format_stock_data['open'] = None
         format_stock_data['high'] = None
         format_stock_data['low'] = None
         format_stock_data['close'] = None
+        format_stock_data['change'] = None
+        format_stock_data['change_percent'] = None
 
     return format_stock_data
 
