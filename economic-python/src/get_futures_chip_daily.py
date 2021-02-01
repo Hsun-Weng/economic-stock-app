cab2bca3b8d7fa87bcdbe98fb0bff1f1867813f9
weihsun.weng <weihsun@pmi-ai.com>
2021-02-01 18:57:37 +0800

    Udpate Parsing chips

--- economic-python/src/get_futures_chip_daily.py
+++ economic-python/src/get_futures_chip_daily.py
@@ -1,10 +1,11 @@
 import requests
 import db_params
-from mysql.connector import Error
 import csv
 from datetime import datetime
 
-investor_code_dict = {"自營商": "D", "投信": "IT", "外資及陸資": "FI"}
+investor_code_dict = {"自營商": "D", "投信": "IT", "外資及陸資": "FI", "非法人": "RI"}
+futures_chip_collection = db_params.get_mongo_db()['futures_chip']
+futures_collection = db_params.get_mongo_db()['futures']
 
 def run_get_data(date):
     url = 'https://www.taifex.com.tw/cht/3/dlFutContractsDateDown'
@@ -38,14 +39,19 @@ def parse_response_text(text_content, futures_code):
     mongo_data_dict = {}
     mongo_data_dict['futures_code'] = futures_code
     investor_chip = []
+    date = None
     for futures_chip_raw_data in csv.reader(text_content.splitlines()[1:]): # 跳過第一行標題
         futures_chip_data = format_futures_chip_data(futures_chip_raw_data)
-        mongo_data_dict['date'] = futures_chip_data.pop('date') # 取得日期刪除子物件key: date
+        date = futures_chip_data.pop('date') # 取得日期刪除子物件key: date
         investor_chip.append(futures_chip_data) # 加入list
+    ## 計算散戶資料
+    investor_chip.append(calculate_retail_investor_data(futures_code, date, investor_chip))
 
     if(len(investor_chip) > 0):
+        mongo_data_dict['date'] = date
         mongo_data_dict['investor_chip'] = investor_chip
-        insert_futures_chip_data(mongo_data_dict)
+        print(mongo_data_dict)
+        # futures_chip_collection.insert_one(mongo_data_dict)
         
 # 格式化原始資料
 def format_futures_chip_data(futures_chip_raw_data):
@@ -62,14 +68,33 @@ def format_futures_chip_data(futures_chip_raw_data):
     format_futures_chip_data['open_interest_short_lot'] = int(futures_chip_raw_data[11])
     format_futures_chip_data['open_interest_short_amount'] = int(futures_chip_raw_data[12]) * 1000
     return format_futures_chip_data
+
+
+#  補上散戶資料
+def calculate_retail_investor_data(futures_code, date, corporation_chip_data):
+    # 法人多單
+    corporation_long_lot = sum(map(lambda futures: futures['open_interest_long_lot'], corporation_chip_data))
+    # 法人空單
+    corporation_short_lot = sum(map(lambda futures: futures['open_interest_short_lot'], corporation_chip_data))
+
+    total_open_interest_lot = get_total_open_interest_lot(futures_code, date)
+
+    retail_investor_chip_data = {}
+    retail_investor_chip_data['investor_code'] = 'RI'
+    retail_investor_chip_data['open_interest_long_lot'] = total_open_interest_lot - corporation_long_lot
+    retail_investor_chip_data['open_interest_short_lot'] = total_open_interest_lot - corporation_short_lot
+
+    return retail_investor_chip_data
+
+### 查詢全市場未平倉口數
+def get_total_open_interest_lot(futures_code, date):
+    futures_list = list(futures_collection.find({'futures_code': futures_code, "date": {'$gte': datetime(date.year, date.month, date.day, 0, 0, 0)
+    , '$lte': datetime(date.year, date.month, date.day, 23, 59, 59)}, 'open_interest_lot': {'$ne': None}}, {'open_interest_lot': 1}))
+        
+    if len(futures_list) > 0:
+        return sum(map(lambda futures: futures['open_interest_lot'], futures_list))
+    return 0
     
-
-def insert_futures_chip_data(data):
-    mongo_client = db_params.get_mongo_client()
-    db = mongo_client['economic']
-    stock_index_collection = db['futures_chip']
-    stock_index_collection.insert_one(data)
-
 if __name__ == "__main__":
     current_time = datetime.now()
     # 當日
